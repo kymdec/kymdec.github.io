c109bd54ce1565dbdc80824cc02834c7ee240c64
kymdec <84751677+kymdec@users.noreply.github.com>
2021-05-25 21:41:55 -0500

    Delete cmp directory

--- cmp/mi-footer.js
+++
@@ -1,8 +0,0 @@
-class MiFooter extends HTMLElement{
-    connectedCallback() {
-        this.innerHTML = /* html */
-        `Copyright &copy; 2021 Kymberly Decena Escorsa.`;
-
-    }
-}
-customElements.define("mi-footer",MiFooter);

00db8f6535d7c074c435548950cbcba955292a59
kymdec <84751677+kymdec@users.noreply.github.com>
2021-05-25 22:26:51 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/* Initializa Firebase con la configuración del proyecto.
+* Revisa la configuracion en tu servidor de Firebase */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBGOkLwXH7eDVI1TLhGTeinUQ2WsTK7b4Y",
+    authDomain: "autecv50.firebaseapp.com",
+    projectId: "autecv50",
+    storageBucket: "autecv50.appspot.com",
+    messagingSenderId: "941516883675",
+    appId: "1:941516883675:web:d7bc3b7a8a576cc1db6efe",
+    measurementId: "G-XTHZ9T1GY1"
+});
